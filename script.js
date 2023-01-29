fa46d9c6e50eff22e8234b0157184cad256ec8f4
Abdul Mobin Fata <Fatapjr2020@gmail.com>
2023-01-29 19:27:32 +0330

    F.2

---
+++ script.js
@@ -0,0 +1,8 @@
+// Typing animation
+
+var typed = new Typed(".hero--dynamic-text", {
+    strings: ["Devoloper", "Freelancer", "Youtuber", "Designer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+      });
